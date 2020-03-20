fcd0a5b3ad5c3ca5619c8fc7274b429c1798d2ce
rpuertolas <62300923+rpuertolas@users.noreply.github.com>
2020-03-20 17:07:26 +0100

    Add files via upload

---
+++ ejemplo_cliente.c
@@ -0,0 +1,89 @@
+﻿/***************************************************************************
+                          main.c  -  client
+                             -------------------
+    begin                : lun feb  4 16:00:04 CET 2002
+    copyright            : (C) 2002 by A. Moreno
+    email                : amoreno@euss.es
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+
+#define REQUEST_MSG_SIZE	1024
+#define REPLY_MSG_SIZE		500
+#define SERVER_PORT_NUM		5001
+
+
+
+ /************************
+*
+*
+* tcpClient
+*
+*
+*/
+int main(int argc, char *argv[]){
+	struct sockaddr_in	serverAddr;
+	char	    serverName[] = "127.0.0.1"; //Adreça IP on est� el servidor
+	int			sockAddrSize;
+	int			sFd;
+	int			mlen;
+	int 		result;
+	char		buffer[256];
+	char		missatge[] = "#1";
+
+	/*Crear el socket*/
+	sFd=socket(AF_INET,SOCK_STREAM,0);
+
+	/*Construir l'adreça*/
+	sockAddrSize = sizeof(struct sockaddr_in);
+	bzero ((char *)&serverAddr, sockAddrSize); //Posar l'estructura a zero
+	serverAddr.sin_family=AF_INET;
+	serverAddr.sin_port=htons (SERVER_PORT_NUM);
+	serverAddr.sin_addr.s_addr = inet_addr(serverName);
+
+	/*Conexió*/
+	result = connect (sFd, (struct sockaddr *) &serverAddr, sockAddrSize);
+	if (result < 0)
+	{
+		printf("Error en establir la connexió\n");
+		exit(-1);
+	}
+	printf("\nConnexió establerta amb el servidor: adreça %s, port %d\n",	inet_ntoa(serverAddr.sin_addr), ntohs(serverAddr.sin_port));
+
+	/*Enviar*/
+	strcpy(buffer,missatge); //Copiar missatge a buffer
+	result = write(sFd, buffer, strlen(buffer));
+	printf("Missatge enviat a servidor(bytes %d): %s\n",	result, missatge);
+
+	/*Rebre*/
+	result = read(sFd, buffer, 256);
+	printf("Missatge rebut del servidor(bytes %d): %s\n",	result, buffer);
+
+	/*Tancar el socket*/
+	close(sFd);
+
+	return 0;
+	}
